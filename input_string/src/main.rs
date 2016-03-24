8912733dfaf619ba227e281ee6177d87381424ff
Amit Saha <amitsaha.in@gmail.com>
2016-03-24 14:49:42 +1100

    input_string

--- input_string/src/main.rs
+++ input_string/src/main.rs
@@ -2,7 +2,10 @@ use std::io;
 
 fn main() {
     let mut line: String = String::new();
-    let res: io::Result<usize> = io::stdin().read_line(&mut line);
+    let stdin: io::Stdin = io::stdin();
+    let res: io::Result<usize>;
+
+    res = stdin.read_line(&mut line);
     if res.is_ok() {
         // Unwrap the result to extract the value
         let nbytes: usize = res.unwrap();
